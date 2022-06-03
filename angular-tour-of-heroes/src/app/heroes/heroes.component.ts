fe1ddd524cf815836c4c3151148b472b47e4fb6d
Ivaylo Mutafchiev <69365693+imutafchiev94@users.noreply.github.com>
2022-06-03 15:49:58 +0300

    Add Hero Service
    Add Hero Service to get data by asynchronous operation using observable

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,21 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private HeroService: HeroService) { }
 
-  heroes = HEROES; 
+  heroes: Hero[] = []; 
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   selectedHero?: Hero;
   onSelect(hero: Hero) : void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.HeroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
